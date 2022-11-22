2703f18fbfc9e24c1feb77bb6d1296d647325090
John Grey <johngrey4296@gmail.com>
2022-11-22 01:05:18 +0000

    [restructure]

--- code_analysis/util/lua/handle_lua.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env python3
-from code_analysis.specific.lua import parser
-from code_analysis.specific.luad import lua_data as LD
-
-def handle_lua(data, filename):
-    pass
--- code_analysis/util/natlang.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python3
-"""
-Utilities for working with natural language
-and spacy/textblob representations of them.
-ie: tree / POS utils etc
-
-"""
