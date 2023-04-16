600fe2c6717f321675cd5924e6c4e8855678574f
John Grey <johngrey4296@gmail.com>
2023-04-16 05:09:30 +0100

    [refactor] binary parsing
    --------------------------------------------------
    Warnings:
    
    --------------------------------------------------
    Basic Explanation:
    
    --------------------------------------------------
    Outstanding issues:
    
    --------------------------------------------------
    Notes:

--- code_analysis/doot_tasks/binary.py
+++ code_analysis/doot_tasks/binary.py
@@ -38,17 +38,39 @@ from code_analysis.mixins.dataset_globber import CADatasetGlobber
 from code_analysis.structs.binary import infinity
 
 class BiowareInfinityBinaryGlobber(CADatasetGlobber, infinity.BioWareInfinityBinaryMixin):
+    """
+    extract files from infinity engine .bif 's, and parse them
+    """
 
-    select_tags = ["bioware", "infinity"]
+    select_tags    = ["bioware", "infinity_engine"]
+    select_types   = [0x03ed, 0x03ee, 0x03ef, 0x03f0, 0x03f3, 0x03f4, 0x03f6, 0x03f8, 0x03f9, 0x03fa, 0x03fe, 0x0403, 0x0803]
+    needs_key_file = [".bif"]
 
-    def __init__(self, name="binary::bioware", locs=None, roots=None):
-        super().__init__(name, locs, roots or [locs.data], exts=[".key", ".tlk", ".gff", ".d", ".2da"], rec=True)
+    def __init__(self, name="binary::bioware", locs=None, roots=None, exts=None, result_tag="binary"):
+        super().__init__(name, locs, roots or [locs.data], exts=exts or [".key", ".bif", ".tlk", ".gff", ".dlg", ".2da"], rec=True)
+        self.current_key_file             = None
+        self.resource_desc_lookup = dict()
+        self.result_tag           = result_tag
 
     def subtask_detail(self, task, fpath):
+        parser      = None
+        handlers    = []
+        marker      = self.find_marker(fpath)
         match fpath.suffix.lower():
             case ".key":
-                parser = self.build_key_v1_format()
-            case ".tlk" | ".gff" | ".d" | ".2da":
+                parser  = self.build_key_v1_format()
+                handlers.append(self.report_resources)
+                handlers.append(self.setup_key_lookup)
+            case ".bif":
+                parser   = self.build_bif_v1_format()
+                handlers.append(self.write_bif_contents)
+            case ".dlg":
+                parser   = self.build_dlg_v1_format()
+                handlers.append(self.write_dlg_contents)
+            case ".tlk":
+                parser = self.build_tlk_v1_format()
+                handlers.append(self.write_tlk_contents)
+            case ".tlk" | ".gff" | ".2da":
                 logging.debug("Bioware Binary parser not implemented for: %s", fpath)
                 return
             case _:
@@ -56,45 +78,103 @@ class BiowareInfinityBinaryGlobber(CADatasetGlobber, infinity.BioWareInfinityBin
                 return
 
         task.update({
-            "actions" : [ (self.parse_file, [fpath, parser])],
+            "actions" : [
+                (self.make_linked_dataset, [self.result_tag, fpath]), # -> dataset
+                (self.find_key, [fpath]),
+                (self.parse_file, [fpath, parser, handlers]),
+                ],
         })
         return task
 
-    def parse_file(self, fpath:pl.Path, parser:Construct.Struct):
-        logging.info("-- Binary Parsing File: %s", fpath)
+    def find_key(self, fpath):
+        if fpath.suffix.lower() not in self.needs_key_file:
+            return
+
+        marker = self.find_marker(fpath)
+        if marker is None:
+            raise FileNotFoundError(f"Can't find dataset marker for: {fpath}")
+
+        keys    = list(marker.parent.rglob("*.key"))
+        if bool(keys) and keys[0] != self.current_key_file:
+            self.parse_file(keys[0], self.build_key_v1_format(), [self.setup_key_lookup])
+            self.current_key_file = keys[0]
+            return
+        elif bool(keys) and keys[0] == self.current_key_file:
+            return
+
+        logging.warning("Can't Find a key file for: %s", fpath)
+        return False
+
+    def parse_file(self, fpath:pl.Path, parser:Construct.Struct, handlers:list[Callable]):
         try:
+            logging.info("-- Binary Parsing File: %s", fpath)
             result = parser.parse_file(fpath)
+            logging.info("-- Binary Parsing Finished: %s", fpath)
+            for handler in handlers:
+                handler(fpath, result)
         except Exception as err:
-            logging.error(f"Binary Parse Failed: %s : %s", fpath, err)
+            logging.error(f"-- Binary Parse Failed: %s : %s", fpath, err)
+            raise err from err
 
-        self.report_resources(result, self.to_mirror(fpath))
-        logging.info("-- Binary Parsing Finished: %s", fpath)
-
-    def report_resources(self, data:Construct.Struct, output:pl.Path):
-        logging.info("Reporting Results: %s", output)
+    def report_resources(self, fpath, data:Construct.Struct):
+        output = self.to_linked_data_path(fpath.parent, self.result_tag)
+        logging.info("Reporting Results to: %s", output)
         assert(all([x in data for x in ["header", "bif_descs", "res_descs"]]))
-        select = [0x03ed, 0x03ee, 0x03ef, 0x03f0, 0x03f3, 0x03f4, 0x03f6, 0x03f8, 0x03f9, 0x03fa, 0x03fe, 0x0403, 0x0803]
         header = data.header
         bifs   = data.bif_descs
         recs   = data.res_descs
 
-        results = []
+        results       = []
         relevant_bifs = set()
         for res in recs:
-            if res.type not in select:
+            res_key  = res.resource_key
+            bif_name = bifs[res.resource_key.bif].name
+            bif_key  = res.resource_key.file
+            res_name = res.name
+            res_type = self.file_types.get(res.type)
+            results.append(f"{bif_name} : {res_key.bif}_{res_key.tile}_{res_key.file} : {res_name}{res_type}")
+
+            if res.type not in self.select_types:
+                relevant_bifs.add(bif_name)
                 continue
-            bif_name  = bifs[res.index.bif].name.str
-            bif_index = res.index.file
-            res_name  = res.name
-            res_type  = self.file_types.get(res.type)
 
-            relevant_bifs.add(bif_name)
-            results.append(f"{bif_name} : {bif_index:<5} : {res_name}{res_type}")
+        (output / "key.relevant").write_text("\n".join(relevant_bifs))
+        (output / "key.resources").write_text("\n".join(results))
 
-        if not output.parent.exists():
-            output.parent.mkdir(parents=True)
+    def setup_key_lookup(self, fpath, data):
+        header = data.header
+        recs   = data.res_descs
+        bifs   = data.bif_descs
+        for res in recs:
+            res_key  = res.resource_key
+            bif_name = pl.Path(bifs[res.resource_key.bif].name).stem.lower()
+            res_name = res.name
+            res_type = self.file_types.get(res.type)
+            self.resource_desc_lookup[f"{bif_name}_{res_key.file}".lower()] = f"{res_name}{res_type}"
 
-        output.with_suffix(".relevant").write_text("\n".join(relevant_bifs))
-        output.with_suffix(".resources").write_text("\n".join(results))
+    def write_bif_contents(self, fpath, data):
+        output = self.to_linked_data_path(fpath.with_suffix(""), self.result_tag)
+        logging.info("-- Writing Raw Bif Contents: %s : %s", data.header.file_entries_count, output)
+        for entry in data.file_entries:
+            key = entry.resource_key
 
+            true_name = self.resource_desc_lookup.get(f"{fpath.stem}_{key.file}".lower(), None)
+            if true_name is None:
+                logging.warning("Resource Not Found in key resources: %s : %s : %s", key.bif, key.tile, key.file)
+                true_name = f"{fpath.stem}_{key.file}.raw"
 
+            (output / true_name).write_bytes(bytes(entry.data))
+
+    def write_dlg_contents(self, fpath, data):
+        output = self.to_linked_data_path(fpath.with_suffix(""), self.result_tag)
+        logging.info("-- Writing Raw DLG Contents: %s : %s", fpath, output)
+        logging.info("State Count: %s", data.header.state_count)
+        logging.info("Transition Count: %s", data.header.transition_count)
+        logging.info("State Trigger Count: %s", data.header.state_trigger_count)
+        logging.info("Transition Trigger Count: %s", data.header.transition_trigger_count)
+        logging.info("Action Count: %s", data.header.action_count)
+
+    def write_tlk_contents(self, fpath, data):
+        logging.info("Parsed TLK")
+        logging.info("Lang: %s", data.header.language)
+        logging.info("Entries: %s", data.header.entry_count)
--- code_analysis/structs/binary/infinity.py
+++ code_analysis/structs/binary/infinity.py
@@ -100,15 +100,9 @@ class BioWareInfinityBinaryMixin:
     """
     Word   = C.Int16ul
     DWord  = C.Int32ul
-    # Offset string
-    OffStr = C.Struct(
-        "_pos" / C.Tell,
-        C.Seek(C.this._.name_offset),
-        "str"    / C.CString("ascii"),
-        C.Seek(C.this._pos)
-        )
     file_types = infinity_file_types
 
+    ##-- top level
     def build_key_v1_format(self) -> C.Struct:
         """
         Build a construct parser for infinity engine KEY format file
@@ -124,6 +118,65 @@ class BioWareInfinityBinaryMixin:
                         C.Seek(C.this.header.res_offset),
                         "res_descs" / self._key_v1_resource_description[C.this.header.res_num]
                         )
+
+    def build_bif_v1_format(self) -> C.Struct:
+        """
+        https://gibberlings3.github.io/iesdp/file_formats/ie_formats/bif_v1.htm
+        """
+        return C.Struct("header" / self._bif_v1_header,
+                        C.Seek(C.this.header.offset),
+                        "file_entries" / self._bif_v1_entry[C.this.header.file_entries_count],
+                        C.Check(C.len_(C.this.file_entries) == C.this.header.file_entries_count),
+                        "tileset_entries" / self._bif_v1_tileset[C.this.header.tileset_entries_count],
+                        C.Check(C.len_(C.this.tileset_entries) == C.this.header.tileset_entries_count),
+                        )
+
+    def build_bifc_v1_format(self) -> C.Struct:
+        return C.Struct("header" / self._bif_compressed_v1_header,
+                        "blocks" / C.GreedyRange(self._bif_compressed_v1_block),
+                        )
+
+    def build_tlk_v1_format(self) -> C.Struct:
+        """
+        https://gibberlings3.github.io/iesdp/file_formats/ie_formats/tlk_v1.htm
+        """
+        return C.Struct("header" / self._tlk_v1_header,
+                        C.Seek(18),
+                        "entries" / self._tlk_v1_entries[C.this.header.entry_count],
+                        )
+
+    def build_dlg_v1_format(self) -> C.Struct:
+        """
+        https://gibberlings3.github.io/iesdp/file_formats/ie_formats/dlg_v1.htm
+        """
+        return C.Struct("header"              / self._dlg_v1_header,
+                        "state_table"         / C.Pointer(C.this.header.state_offset,              self._dlg_v1_state[C.this.header.state_count]),
+                        "transition_table"    / C.Pointer(C.this.header.transition_offset,         self._dlg_v1_transition[C.this.header.transition_count]),
+                        "state_triggers"      / C.Pointer(C.this.header.state_trigger_offset,      self._dlg_v1_data[C.this.header.state_trigger_count]),
+                        "transition_triggers" / C.Pointer(C.this.header.transition_trigger_offset, self._dlg_v1_data[C.this.header.transition_trigger_count]),
+                        "actions"             / C.Pointer(C.this.header.action_offset,             self._dlg_v1_data[C.this.header.action_count])
+                        )
+
+    def build_2da_format(self) -> C.Struct:
+        """
+        https://gibberlings3.github.io/iesdp/file_formats/ie_formats/2da.htm
+        TODO detect 0xff for encryption
+        """
+        raise NotImplementedError()
+
+    def build_src_format(self) -> C.Struct:
+        """
+        https://gibberlings3.github.io/iesdp/file_formats/ie_formats/src.htm
+        """
+        return C.Struct("entry_count" / self.DWord,
+                        "entries" / C.Struct("str_ref" / self.DWord,
+                                             "weight"  / self.DWord)[C.this.entry_count]
+                        )
+
+    ##-- end top level
+
+    ##-- components
+
     @property
     def _key_v1_header(self) -> C.Struct:
         return C.Struct(C.Const(b"KEY"), C.Padding(1),
@@ -140,124 +193,164 @@ class BioWareInfinityBinaryMixin:
                         "id" / C.Computed(C.this._index),
                         "file_len"    / self.DWord,
                         "name_offset" / self.DWord,
-                        "name"        / self.OffStr,
+                        "name"        / C.Pointer(C.this._.name_offset, C.CString("ascii")),
                         "name_len"    / self.Word,
                         "location"    / C.FlagsEnum(self.Word, data=1, cache=2, cd1=4, cd2=8, cd3=16, cd5=24, cd6=32),
-            )
+                        )
 
     @property
     def _key_v1_resource_description(self) -> C.Struct:
-        return C.Struct("id" / C.Computed(C.this._index),
-                        "name"  / C.PaddedString(8, "ascii"),
-                        "type"  / self.Word,
-                        "index" / C.ByteSwapped(C.BitStruct(
-                            "bif" / C.BitsInteger(12),
-                            "tile" / C.BitsInteger(6),
-                            "file" / C.BitsInteger(14),
-                        )))
+        return C.Struct("id"           / C.Computed(C.this._index),
+                        "name"         / C.PaddedString(8, "ascii"),
+                        "type"         / self.Word,
+                        "resource_key" / self._key_v1_resource_key
+                        )
 
-    def TODO_build_bif_v1() -> C.Struct:
-        """
-        https://gibberlings3.github.io/iesdp/file_formats/ie_formats/bif_v1.htm
-        """
-        raise NotImplementedError()
+    @property
+    def _key_v1_resource_key(self) -> C.Struct:
+        return C.ByteSwapped(C.BitStruct(
+            "bif"  / C.BitsInteger(12),
+            "tile" / C.BitsInteger(6),
+            "file" / C.BitsInteger(14)
+        ))
 
     @property
     def _bif_v1_header(self) -> C.Struct:
-        # 4 char: BIFF
-        # 4 char: V1
-        # 4 dword : file count
-        # 4 dword : tile count
-        # 4 dword : offset from start of file (SoF) to file entries
-        raise NotImplementedError()
+        return C.Struct(C.Const(b"BIFF"), C.Const(b'V1  '),
+                        "file_entries_count"    / self.DWord,
+                        "tileset_entries_count" / self.DWord,
+                        "offset"                / self.DWord, # from start of file
+                        )
 
     @property
     def _bif_v1_entry(self) -> C.Struct:
-        # 4 dword : resource key -> in chitin.key
-        # 4 dword : offset from SoF to data
-        # 4 dword : size of data
-        # 2 word : file type
-        # 2 word : unknown
-        raise NotImplementedError()
+        return C.Struct("resource_key" / self._key_v1_resource_key,
+                        "offset"       / self.DWord, # from start of file
+                        "size"         / self.DWord,
+                        "type"         / self.Word,
+                        self.Word,
+                        "data"         / C.Pointer(C.this.offset, C.Array(C.this.size, C.Byte)),
+                        )
 
     @property
-    def _bif_v1_tile(self) -> C.Struct:
-        # 4 dword : resource key -> in chitin.key
-        # 4 dword : offset from SoF to data
-        # 4 dword : tile count
-        # 4 dword : tile size
-        # 2 word : type (always 0x3eb)
-        # 2 word : unknown
-        raise NotImplementedError()
+    def _bif_v1_tileset(self) -> C.Struct:
+        return C.Struct("resource_key" / self._key_v1_resource_key,
+                        "offset"       / self.DWord,
+                        "count"        / self.DWord,
+                        "size"         / self.DWord,
+                        "type"         / C.Const(b'\x3eb'),
+                        self.Word
+                        # Data is ignored for now
+                        )
 
-    def TODO_build_tlk_v1(self) -> C.Struct:
-        """
-        https://gibberlings3.github.io/iesdp/file_formats/ie_formats/tlk_v1.htm
-        """
-        raise NotImplementedError()
+    @property
+    def _bif_compressed_v1_header(self) -> C.Struct:
+        return C.Struct(C.Const(b"BIFC"), c.Const(b'V1.0'),
+                        "size" / self.DWord
+                        )
+
+    @property
+    def _bif_compressed_v1_block(self) -> C.Struct:
+        return C.Struct("decompressed_size" / self.DWord,
+                        "compressed_size"   / self.DWord,
+                        "data"              / C.FixedSized(C.this.compressed_size, C.Compressed(C.GreedyBytes, "zlib")),
+                        )
 
     @property
     def _tlk_v1_header(self) -> C.Struct:
-        # 4 char : "TLK "
-        # 4 char : "V1  "
-        # 2 word : language id
-        # 4 dword : number of strref's
-        # 4 dword : offset from SoF to data
-        raise NotImplementedError()
+        # 4 dword : offset from SoF to string data
+        return C.Struct(C.Const(b"TLK "), C.Const(b"V1  "),
+                        "language"      / self.Word,
+                        "entry_count"   / self.DWord,
+                        "string_offset" / self.DWord,
+                        )
 
     @property
     def _tlk_v1_entries(self) -> C.Struct:
-        # 2 word : bit field
-        # 8 resref : resource name of sound
-        # 4 dword : volume variance
-        # 4 dword : pitch variance
-        # 4 dword : offset from start of strref section
-        # 4 dword : length of string
-        raise NotImplementedError()
+        metadata = C.FlagsEnum(self.Word,
+                               message_data=1,
+                               sound=2,
+                               standard_message=4,
+                               token=8
+                               )
+
+        return C.Struct("index"           / C.Computed(C.this._index),
+                        "position"        / C.Tell,
+                        "metadata"        / metadata,
+                        "sound"           / self._tlk_v1_resref,
+                        "volume"          / self.DWord,
+                        "pitch"           / self.DWord,
+                        "relative_offset" / self.DWord,
+                        "length"          / self.DWord,
+                        "full_offset"     / C.Computed(C.this._.header.string_offset + C.this.relative_offset),
+                        # C.Probe(),
+                        "data"            / C.Pointer(C.this.full_offset, C.FixedSized(C.this.length, C.NullStripped(C.GreedyString("utf-8")))),
+                        # C.Probe(C.this.data),
+                        )
 
     @property
-    def _tlk_v1_strings(self) -> C.Struct:
-        # ascii encoded. not allways NULL terminated
-        raise NotImplementedError()
+    def _tlk_v1_strref(self) -> C.Struct:
+        return C.Array(4, C.Byte)
 
-    def TODO_build_dlg_v1(self) -> C.Struct:
-        """
-        https://gibberlings3.github.io/iesdp/file_formats/ie_formats/dlg_v1.htm
-        """
-        raise NotImplementedError()
+    @property
+    def _tlk_v1_resref(self) -> C.Struct:
+        return C.FixedSized(8, C.Byte)
 
     @property
     def _dlg_v1_header(self) -> C.Struct:
-        raise NotImplementedError()
+       return C.Struct(C.Const(b"DLG "), C.Const(b"V1.0"),
+                       "state_count"               / self.DWord,
+                       "state_offset"              / self.DWord,
+                       "transition_count"          / self.DWord,
+                       "transition_offset"         / self.DWord,
+                       "state_trigger_offset"      / self.DWord,
+                       "state_trigger_count"       / self.DWord,
+                       "transition_trigger_offset" / self.DWord,
+                       "transition_trigger_count"  / self.DWord,
+                       "action_offset"             / self.DWord,
+                       "action_count"              / self.DWord,
+                       # "interruption"              / C.Enum(self.DWord, ),
+                    )
 
     @property
-    def _dlg_v1_state_table(self) -> C.Struct:
-        raise NotImplementedError()
+    def _dlg_v1_state(self) -> C.Struct:
+        return C.Struct("actor_response_string_reference" / self._tlk_v1_strref,
+                        "transition_index"  / self.DWord,
+                        "transition_count"  / self.DWord, # state transitions = transition_index -> (transition_index + transition_count) - 1
+                        "trigger_index"     / self.DWord) # 0xFFFFFFFF if no triggers
 
     @property
-    def _dlg_v1_transition_table(self) -> C.Struct:
-        raise NotImplementedError()
+    def _dlg_v1_transition(self) -> C.Struct:
+        metadata = C.FlagsEnum(self.DWord,
+                               associated_text=1,
+                               trigger=2,
+                               action=4,
+                               terminates=8,
+                               journal=16,
+                               interrupt=32,
+                               add_unsolved_quest_journal=64,
+                               add_journal=128,
+                               add_solved_quest_journal=256,
+                               delayed_script_execution=512,
+                               clear_actions=1024,
+                               )
+
+        return C.Struct(
+            "metadata"                    / metadata,
+            "transition_string_reference" / self._tlk_v1_strref,
+            "journal_string_reference"    / self._tlk_v1_strref,
+            "trigger_index"               / self.DWord,
+            "action_index"                / self.DWord,
+            "next_dlg_resource_ref"       / self._tlk_v1_resref,
+            "next_state_index"            / self.DWord
+        )
 
     @property
-    def _dlg_v1_state_triggers(self) -> C.Struct:
-        raise NotImplementedError()
+    def _dlg_v1_data(self) -> C.Struct:
+        # state trigger / transition triggr / action
+        return C.Struct("offset" / self.DWord,
+                        "length" / self.DWord,
+                        "text"   / C.Pointer(C.this.offset, C.FixedSized(C.this.length, C.NullStripped(C.GreedyString("ascii")))),
+                        )
 
-    @property
-    def _dlg_v1_transition_triggers(self) -> C.Struct:
-        raise NotImplementedError()
-
-    @property
-    def _dlg_v1_action_table(self) -> C.Struct:
-        raise NotImplementedError()
-
-    def TODO_build_2da_format() -> C.Struct:
-        """
-        https://gibberlings3.github.io/iesdp/file_formats/ie_formats/2da.htm
-        """
-        raise NotImplementedError()
-
-    def TODO_build_src_format() -> C.Struct:
-        """
-        https://gibberlings3.github.io/iesdp/file_formats/ie_formats/src.htm
-        """
-        raise NotImplementedError()
+    ##-- end components
