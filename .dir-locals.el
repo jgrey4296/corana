a3d3e1b9a8c2b8c8abf2e8cee41cd9ccd6717a9a
John Grey <johngrey4296@gmail.com>
2024-11-23 00:26:35 +0000

    [chore]:

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,4 +1,4 @@
 ;;; Directory Local Variables
 ;;; For more information see (info "(emacs) Directory Variables")
 
-((python-mode . ((eval . (+jg-python-activate-venv-and-conda)))))
+;; ((python-mode . ((eval . (+jg-python-activate-venv-and-conda)))))
